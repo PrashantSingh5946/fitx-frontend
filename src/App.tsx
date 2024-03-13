53cede100818dd0a2c0919cdf9b293ade996161f
Animesh Raj <animeshraj223@gmail.com>
2024-03-13 22:00:52 +0530

    desktopUI Dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Profile from "./pages/profile/page";
 import Camera from "./pages/camera/page";
 
 import Login from "./pages/login/Login";
-
+import DashUI from "./pages/desktopDashboard/DashUI";
 import {
   BrowserRouter,
   BrowserRouter as Router,
@@ -28,6 +28,7 @@ function App() {
           <Route path="/search" element={<Search />} />
           <Route path="/camera" element={<Camera />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/dash" element={<DashUI />} />
         </Route>
 
         <Route path="/login" element={<Login />} />
