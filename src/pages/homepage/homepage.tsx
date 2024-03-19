5a8878d4a44a042c56163d2b4b62b36962621245
Prashant Singh <octanesingh@gmail.com>
2024-03-19 22:16:36 +0530

    Removing styles

--- src/pages/homepage/homepage.tsx
+++ src/pages/homepage/homepage.tsx
@@ -9,9 +9,8 @@ import {
 import { Link } from "react-router-dom";
 import styles from "./styles.module.css";
 
-import "./Home.css";
-import "./nicepage.css";
-
+// import "./Home.css";
+// import "./nicepage.css";
 import dietImage2 from "./images/diet2.jpeg";
 import dietImage from "./images/diet.jpeg";
 
